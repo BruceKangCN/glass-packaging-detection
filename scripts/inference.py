ab7913ed54ca8bfbe287c59b4fd76825ce2f796c
Bruce Kang <brucekangcn@gmail.com>
2026-08-06 14:49:20 +0800

    add ipython detection for inference.py

--- scripts/inference.py
+++ scripts/inference.py
@@ -9,7 +9,14 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from ultralytics import YOLO
 
-mpl.use("QtAgg")
+try:
+    # 检测 IPython 环境以判断是否处于笔记本模式中
+    get_ipython() # type: ignore
+    # 若是，则使用 widget 渲染后端
+    mpl.use("widget")
+except NameError:
+    # 若未处于笔记本环境中，则使用 QtAgg 渲染后端
+    mpl.use("QtAgg")
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 # %%
